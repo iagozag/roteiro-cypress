71add5ea04a9521dc2db0241d2f26a3daab31b82
iagozag <iagozag@gmail.com>
2024-11-28 17:16:31 -0300

    Testes feitos

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,16 +4,16 @@ describe('TODOMvc App', () => {
   })
 
   it('Insere uma tarefa', () => {
-    cy.visit(''); 
+    cy.visit('');
 
     cy.get('[data-cy=todo-input]')
       .type('TP2 de Engenharia de Software{enter}');
 
     cy.get('[data-cy=todos-list]')
       .children()
-      .should('have.length', 1) 
+      .should('have.length', 1)
       .first()
-      .should('have.text', 'TP2 de Engenharia de Software'); 
+      .should('have.text', 'TP2 de Engenharia de Software');
   });
 
   it('Insere e deleta uma tarefa', () => {
@@ -36,7 +36,7 @@ describe('TODOMvc App', () => {
   });
 
   it('Filtra tarefas completas e ativas', () => {
-    cy.visit(''); 
+    cy.visit('');
 
     cy.get('[data-cy=todo-input]')
       .type('TP2 de ES{enter}')
@@ -68,4 +68,64 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  it('Marca todas as tarefas como completas', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('Estudar Cypress{enter}')
+      .type('Revisar código{enter}');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .each(($el) => {
+        cy.wrap($el).click();
+      });
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .each(($el) => {
+        cy.wrap($el).should('be.checked');
+      });
+  });
+
+  it('Limpa todas as tarefas completas', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('Projeto final{enter}')
+      .type('Enviar relatório{enter}');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    cy.get('.clear-completed')
+      .click();
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Enviar relatório');
+  });
+
+  it('Edita uma tarefa existente', () => {
+    cy.visit('');
+  
+    cy.get('[data-cy=todo-input]')
+      .type('Implementar testes{enter}');
+  
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .dblclick();
+  
+    cy.get('.edit')
+      .clear()
+      .type('Corrigir bugs{enter}');
+  
+    cy.get('[data-cy=todos-list] > li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Corrigir bugs');
+  });
+  
 });
